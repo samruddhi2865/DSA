5bb25f2f5d9fa55c9630aa9def61a2d9a70b71b6
samruddhi2865 <samrudhikshirsagar65@gmail.com>
2026-07-02 21:25:10 +0530

    Maximum Subarray

---
+++ Subarray.java
@@ -0,0 +1,16 @@
+public class Subarray {
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either start a new subarray or extend the existing one
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+
+            // Update the maximum sum found so far
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
